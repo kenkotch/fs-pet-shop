555de27e459b20336fe6e5db841d57ea27c77725
Ken Kotch <hello@ken.uno>
2017-09-14 09:39:46 -0600

    test is broken but commands work

--- httpServer.js
+++ httpServer.js
@@ -1,74 +1,34 @@
 'use strict'
 
-const fs = require('fs')
-
-const path = require('path')
-
-const petsPath = path.join(__dirname, 'pets.json')
-
 const http = require('http')
+
+const sourceFile = require('./pets.json')
 
 const port = process.env.PORT || 8000
 
 const server = http.createServer((req, res) => {
   if (req.method === 'GET' && req.url === '/pets') {
-    fs.readFile(petsPath, 'utf8', (err, petsJSON) => {
-      if (err) {
-        console.error(err.stack)
-        res.statusCode = 500
-        res.setHeader('Content-Type', 'text/plain')
-        res.end('Interna Server Error')
-      }
-      res.setHeader('Content-Type', 'application/json')
-      res.end(petsJSON)
-    })
+    res.setHeader('Content-Type', 'application/json')
+    res.statusCode = 200
+    res.end(JSON.stringify(sourceFile))
   } else if (req.method === 'GET' && req.url === '/pets/0') {
-    fs.readFile(petsPath, 'utf8', (err, petsJSON) => {
-      if (err) {
-        console.error(err.stack)
-        res.statusCode = 500
-        res.setHeader('Content-Type', 'text/plain')
-        res.end('Interna Server Error')
-      }
-      let pet = JSON.parse(petsJSON)
-      let petsFound = JSON.stringify(pet[0])
-
-      res.setHeader('Content-Type', 'application/json')
-      res.end(petsFound)
-    })
+    res.setHeader('Content-Type', 'application/json')
+    res.statusCode = 200
+    res.end(JSON.stringify(sourceFile[0]))
   } else if (req.method === 'GET' && req.url === '/pets/1') {
-    fs.readFile(petsPath, 'utf8', (err, petsJSON) => {
-      if (err) {
-        console.error(err.stack)
-        res.statusCode = 500
-        res.setHeader('Content-Type', 'text/plain')
-        res.end('Interna Server Error')
-      }
-      let pet = JSON.parse(petsJSON)
-      let petsFound = JSON.stringify(pet[1])
-
-      res.setHeader('Content-Type', 'application/json')
-      res.end(petsFound)
-    })
+    res.setHeader('Content-Type', 'application/json')
+    res.statusCode = 200
+    res.end(JSON.stringify(sourceFile[1]))
   } else if (req.method === 'GET' && (req.url === '/pets/2' || req.url === '/pets/-1')) {
-    fs.readFile(petsPath, 'utf8', (err, petsJSON) => {
-      if (err) {
-        console.error(err.stack)
-        res.statusCode = 500
-        res.setHeader('Content-Type', 'text/plain')
-        res.end('Interna Server Error')
-      }
-      res.statusCode = 404
-      res.setHeader('Content-Type', 'text/plain')
-      res.end('Not Found')
-    })
-  } else {
-    res.statusCode = 404
     res.setHeader('Content-Type', 'text/plain')
-    res.end('Not found')
+    res.statusCode = 404
+    res.end('Not Found')
+  } else {
+    res.setHeader('Content-Type', 'text/plain')
+    res.statusCode = 404
+    res.end('Not Found')
   }
 })
-
 server.listen(port, () => {
   console.log('Listening on port', port)
 })
